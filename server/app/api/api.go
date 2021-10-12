e2c32be22a9ea9ea1fd3bd58eab2fad0cbf85876
terrycgo <acchn@live.com>
2021-10-12 21:45:30 +0800

    refactor: add auth

--- server/app/api/api.go
+++ server/app/api/api.go
@@ -1,13 +1,11 @@
 package api
 
 import (
-	"github.com/develop1024/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"net/http"
+	"gra/auth"
 	"strconv"
-	"time"
 )
 
 type UserInfo struct {
@@ -79,84 +77,9 @@ func Start() {
 
 	})
 
-	r.POST("/auth", authHandler)
+	r.POST("/auth", auth.AuthHandler)
 
 	r.Run() // listen and serve on 0.0.0.0:8080
-}
-
-type User struct {
-	Username string
-	Password string
-}
-
-// MyClaims 自定义声明结构体并内嵌jwt.StandardClaims
-// jwt包自带的jwt.StandardClaims只包含了官方字段
-// 我们这里需要额外记录一个username字段，所以要自定义结构体
-// 如果想要保存更多信息，都可以添加到这个结构体中
-type MyClaims struct {
-	Username string `json:"username"`
-	jwt.StandardClaims
-}
-
-const TokenExpireDuration = time.Hour * 2
-
-var MySecret = []byte("mykey")
-
-// GenToken 生成JWT
-func GenToken(username string) (string, error) {
-	// 创建一个我们自己的声明
-	c := MyClaims{
-		username, // 自定义字段
-		jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(TokenExpireDuration).Unix(), // 过期时间
-			Issuer:    "my-project",                               // 签发人
-		},
-	}
-	// 使用指定的签名方法创建签名对象
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, c)
-	// 使用指定的secret签名并获得完整的编码后的字符串token
-	return token.SignedString(MySecret)
-}
-
-func authHandler(c *gin.Context) {
-	// 用户发送用户名和密码过来
-	var user User
-	err := c.ShouldBind(&user)
-	if err != nil {
-		c.JSON(http.StatusOK, gin.H{
-			"code": 2001,
-			"msg":  "无效的参数",
-		})
-		return
-	}
-	// 校验用户名和密码是否正确
-
-	if checkUser(user.Username, user.Password) {
-		// 生成Token
-		tokenString, _ := GenToken(user.Username)
-		c.JSON(http.StatusOK, gin.H{
-			"code": 2000,
-			"msg":  "success",
-			"data": gin.H{"token": tokenString},
-		})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"code": 2002,
-		"msg":  "鉴权失败",
-	})
-	return
-}
-
-func checkUser(name string, password string) bool {
-	db := Before()
-	defer db.Close()
-	u := User{}
-	db.Where("name = ?", "name").First(&u)
-	if u.Password == password {
-		return true
-	}
-	return false
 }
 
 func addUser(id int, name string, gender string, hobby string) {
