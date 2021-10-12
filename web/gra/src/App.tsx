4c4c0af15113284f301714f43b88311714e3ba47
terrycgo <acchn@live.com>
2021-10-12 23:51:41 +0800

    feat: add form

--- web/gra/src/App.tsx
+++ web/gra/src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from "./components/header";
 // @ts-ignore
-import { BrowserRouter,Route,Switch } from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 import routers from "./router";
 
 function App() {
-  return (
-      <BrowserRouter>
-          {
-              routers.map(router=>{
-                  return (
-                      <Route
-                          path={router.path}
-                          component = { router.component }
-                      ></Route>
-                  )
-              })
-          }
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            {
+                routers.map(router => {
+                    return (
+                        <Route
+                            path={router.path}
+                            component={router.component}
+                        />
+                    )
+                })
+            }
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- web/gra/src/pages/admin/index.tsx
+++ web/gra/src/pages/admin/index.tsx
@@ -1,10 +1,60 @@
 import React, {Component} from 'react';
 
-class Admin extends Component {
+class Admin extends Component <{
+    navList ?: any
+},
+    {
+        name ?: string
+        password ?: string
+    }
+    > {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            name: '',
+            password: ''
+        };
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleNameChange(event: { target: { name: any; }; }) {
+        this.setState({name: event.target.name});
+
+        console.log(event.target.name ,this.state.name, '111111')
+    }
+
+    handlePasswordChange(event: { target: { password: any; }; }) {
+        this.setState({password: event.target.password});
+    }
+
+    handleSubmit(event: { preventDefault: () => void; }) {
+        alert('提交的名字: ' + this.state.name);
+        alert('提交的密码: ' + this.state.password);
+        event.preventDefault();
+    }
     render() {
         return (
             <div>
-              <h1>admin</h1>
+                {/*<form onSubmit={this.handleSubmit}>*/}
+                {/*    <label>*/}
+                {/*        账号:*/}
+                {/*        <input type="text" onChange={this.handleNameChange} /><br/>*/}
+                {/*        密码:*/}
+                {/*        <input type="text" /><br/>*/}
+                {/*    </label>*/}
+                {/*    <input type="submit" value="提交" />*/}
+                {/*</form>*/}
+                <form action="http://localhost:8080/user" method="post">
+                    username:<input type="text" name="username" ></input><br/>
+                    password:<input type="text" name="password"></input><br/>
+                    <input type="submit" value="登录"></input>
+
+                </form>
+
             </div>
         );
     }
