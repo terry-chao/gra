7bee1cbca85587c0b2cb44c348a58b4b15c6448e
terrycgo <acchn@live.com>
2021-11-08 21:33:59 +0800

    refactor: rename login to index

---
+++ web/gra/src/pages/login/index.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import LoginForm from "../../components/login_form";
+class Index extends Component {
+    render() {
+        return (
+            <div>
+                <LoginForm />
+            </div>
+        );
+    }
+}
+
+export default Index;
